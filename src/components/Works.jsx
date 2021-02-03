245e51305c7a7aef2147d04ea93162d8c1e84db5
MaximeLeBerre <70652404+MaximeLeBerre@users.noreply.github.com>
2021-02-03 16:01:18 +0100

    filter menu

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -18,38 +18,57 @@ function Works() {
   return (
     <>
       <div className="container-main">
-        <button onClick={()=> setFilterCollage(!isFilterActiveCollage) }>Collage</button>
-        <button onClick={()=> setFilterPhoto(!isFilterActivePhoto) }>Photo</button>
-        <button onClick={()=> setFilterPeinture(!isFilterActivePeiture) }>Peinture</button>
-        <button onClick={()=> setFilterVirtual(!isFilterActiveVirtual) }>Réalité virtuelle</button>
-
+        <div className="btn-container">
+          <button className={ isFilterActiveCollage? 'btn-not-active' : 'btn-active'} onClick={() => setFilterCollage(!isFilterActiveCollage)}>
+            <p>Collage</p>
+          </button>
+          <button className={ isFilterActivePhoto? 'btn-not-active' : 'btn-active'} onClick={() => setFilterPhoto(!isFilterActivePhoto)}>
+          <p>Photo</p>
+          </button>
+          <button className={ isFilterActivePeiture? 'btn-not-active' : 'btn-active'} onClick={() => setFilterPeinture(!isFilterActivePeiture)}>
+          <p>Peinture</p>
+          </button>
+          <button className={ isFilterActiveVirtual? 'btn-not-active' : 'btn-active'} onClick={() => setFilterVirtual(!isFilterActiveVirtual)}>
+          <p>Réalité virtuelle</p>
+          </button>
+        </div>
         <div>
           {works
-          .filter((work) => !isFilterActiveCollage || work.kindName === "Collage")
-          .filter((workImage) => !isFilterActivePhoto || workImage.kindName === "Photographie")
-          .filter((workPaint) => !isFilterActivePeiture || workPaint.kindName === "Peinture")
-          .filter((workVirtual) => !isFilterActiveVirtual || workVirtual.kindName === "Réalité virtuelle")
+            .filter(
+              (work) => !isFilterActiveCollage || work.kindName === "Collage"
+            )
+            .filter(
+              (workImage) =>
+                !isFilterActivePhoto || workImage.kindName === "Photographie"
+            )
+            .filter(
+              (workPaint) =>
+                !isFilterActivePeiture || workPaint.kindName === "Peinture"
+            )
+            .filter(
+              (workVirtual) =>
+                !isFilterActiveVirtual ||
+                workVirtual.kindName === "Réalité virtuelle"
+            )
 
+            .map((work) => (
+              <>
+                <div className="work-container">
+                  <div className="work-content-container">
+                    <h1>{[work.name]}</h1>
+                    <div className="work-p-container">
+                      <p>{[work.date]}</p>
+                      <p>/</p>
+                      <p>{[work.artistName]}</p>
+                    </div>
+                    <p>{`${[work.themeName]} / ${[work.kindName]}`}</p>
+                  </div>
 
-          .map((work) => (
-            <>
-            <div className="work-container">
-              <div className="work-content-container">
-                <h1>{[work.name]}</h1>
-                <div className="work-p-container">
-                  <p>{[work.date]}</p>
-                  <p>/</p>
-                  <p>{[work.artistName]}</p>
+                  <img className="image" src={work.picture} alt={work.name} />
                 </div>
-                <p>{`${[work.themeName]} / ${[work.kindName]}`}</p>
-              </div>
-
-              <img className="image" src={work.picture} alt={work.name} />
-              
-            </div>
-            <hr></hr>
-            </>
-          ))}
+                <hr></hr>
+              </>
+            ))}
         </div>
       </div>
     </>
