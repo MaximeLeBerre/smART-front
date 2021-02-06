271dbbb80d3c4fd7c7b4b82454aba91c38e8d78c
MaximeLeBerre <70652404+MaximeLeBerre@users.noreply.github.com>
2021-02-06 12:37:52 +0100

    remove unless classname

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,13 +3,10 @@
 function Landing() {
   return (
     <>
-      <div class="container">
-        <div class="blocHorizontal">
-          <div class="slide ">
+      <div >
+        <div >
+          <div >
             <h1>Welcome</h1>
-          </div>
-          <div class="slide ">
-            <h1>Works</h1>
           </div>
         </div>
       </div>
